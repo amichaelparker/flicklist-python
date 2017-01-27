cd376690e583b3ce713c5a187ef11750a2dd79ed
Adam Parker <amichaelparker@gmail.com>
2017-01-26 19:45:45 -0600

    added input error messages

--- main.py
+++ main.py
@@ -47,7 +47,7 @@ class Index(webapp2.RequestHandler):
         e.g. www.flicklist.com/
     """
 
-    def get(self):
+    def get(self, error=""):
 
         edit_header = "<h3>Edit My Watchlist</h3>"
 
@@ -103,14 +103,18 @@ class AddMovie(webapp2.RequestHandler):
 
         # TODO 2
         # if the user typed nothing at all, redirect and yell at them
-
+        if not new_movie:
+            self.redirect('/?error=Please+enter+a+movie+name.')
 
         # TODO 3
         # if the user wants to add a terrible movie, redirect and yell at them
-
+        for movie in terrible_movies:
+            if new_movie.lower().find(movie.lower()) != -1:
+                self.redirect('/?error=Please+select+a+good+movie.')
 
         # TODO 1
         # 'escape' the user's input so that if they typed HTML, it doesn't mess up our site
+        new_movie = cgi.escape(new_movie)
 
         # build response content
         new_movie_element = "<strong>" + new_movie + "</strong>"
